4ad1e065ed9ae6df65db17c9ad2ad30db54c4efb
Maro <yukmaro@gmail.com>
2024-09-02 17:10:46 +0900

    [UPDATE] 네비게이션 변수 등록

--- src/stores/modalStore.js
+++ src/stores/modalStore.js
@@ -3,6 +3,14 @@ import { defineStore } from 'pinia'
 // use + 스토어 이름 << 컨벤션임
 export const useModalStore = defineStore('modalStore', {
     state: ()=>({
-        
+        // 네비게이션 사이드 바
+        isSidebarMenu:false,
+
+        // 네비게이션
+        isMain:true,
+        isMainBuild:false,
+        isMainLookup:false,
+        isMy:false,
+        isRanking:false,
     }),
 })
