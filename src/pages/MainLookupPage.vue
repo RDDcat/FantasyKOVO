811683aa4f665c4f182c8869a75800db604dec2d
Maro <yukmaro@gmail.com>
2024-09-02 15:02:43 +0900

    [UPDATE] 소스코드 정리

---
+++ src/pages/MainLookupPage.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- 메인페이지에서 유저 팀 정보 조회 페이지 -->
+    <div class="sample">this page for copy</div>
+</template>
+<script>
+import {useCacheStore} from '@/stores/cacheStore'
+
+export default {
+    components: {
+
+    },
+    setup(){
+        const cacheStore = useCacheStore()
+
+        return { cacheStore }
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style scoped>
+
+
+
+</style>
---
+++ src/pages/MyPage.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- 메인 페이지 첫 랜딩 페이지 -->
+    <div class="sample">this page for copy</div>
+</template>
+<script>
+import {useCacheStore} from '@/stores/cacheStore'
+
+export default {
+    components: {
+
+    },
+    setup(){
+        const cacheStore = useCacheStore()
+
+        return { cacheStore }
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style scoped>
+
+
+
+</style>
