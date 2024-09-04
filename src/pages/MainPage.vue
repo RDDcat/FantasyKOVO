ed259160a98d0b66df39ef557ffc1af2c4b80233
Maro <yukmaro@gmail.com>
2024-09-04 16:53:28 +0900

    [UPDATE] 메인 콘텐츠 업데이트

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,15 @@
 <template>
-<!-- 메인 페이지 첫 랜딩 페이지 -->
-    <div class="sample">this page for copy</div>
+<!-- 메인 페이지 첫 랜딩 페이지 -->    
     <NavigationMenu />
+    <div class="flex p-4 md:p-8 lg:p-12">
+        <div class="mx-auto text-2xl mobile:text-lg desktop:text-3xl">
+            반응형 제목
+        </div>
+        <p class="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+            이 텍스트는 화면 크기에 따라 크기가 변합니다.
+        </p>
+    </div>
+    <img class=" px-4 mobile:w-full desktop:w-[800px]" src="@/assets/court.svg" alt="">
 </template>
 <script>
 import NavigationMenu from '@/components/layout/NavigationMenu.vue'
