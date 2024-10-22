2d51605c41c1ea9f91e24e3d3b7e9ed85215f11f
Maro <yukmaro@gmail.com>
2024-10-22 17:34:09 +0900

    [ADD] GA4, g-tag 설정

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import './index.css'
 import router from './routes';
+import VueGtag from "vue-gtag";
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App).use(pinia).use(router).use(VueGtag, {
+    config: { id: "G-WWLRKR3WKZ" }
+}).mount('#app')
