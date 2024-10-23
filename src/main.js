23092815fc5072a19de972951f60bbbcc4f24696
Maro <yukmaro@gmail.com>
2024-10-23 11:30:53 +0900

    [UPDATE] GA4 페이지 이동도 측정하게 수정

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import './index.css'
-import router from './routes';
-import VueGtag from "vue-gtag";
+import router from './routes'
+import VueGtag from "vue-gtag"
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-createApp(App).use(pinia).use(router).use(VueGtag, {
-    config: { id: "G-WWLRKR3WKZ" }
-}).mount('#app')
+const app = createApp(App)
+// 기본 GA ID 사용
+app.use(VueGtag, {
+    config: {
+        id: "G-WWLRKR3WKZ"
+    }
+}, router)
+
+app.use(pinia)
+    .use(router)
+    .mount('#app')
